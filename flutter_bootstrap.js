98126f31476cca88444c1347b5392496bb127d22
juzuk1q <artemtselikov2011@gmail.com>
2025-12-13 00:13:14 +0300

    Fixed fontSize

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"13e658725ddaa270601426d1485636157e38c3
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1448931366"
+    serviceWorkerVersion: "4028806090"
   }
 });
